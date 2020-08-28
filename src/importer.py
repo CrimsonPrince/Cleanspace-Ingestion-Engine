fddca01545581e92bced4fab47588d01ed88c1be
CrimsonPrince <CrimsonPrince@protonmail.com>
2020-08-28 17:36:52 +0100

    Fixed Importer
    Signed-off-by: r4 <kingr4@protonmail.ch>

--- src/importer.py
+++ src/importer.py
@@ -1,16 +1,25 @@
 import ndjson
 import pprint
 import pymongo
+from pathlib import Path
 
 client = pymongo.MongoClient("mongodb://fyp:KH9jYNsWsCiaUv@54.74.63.10")
 db = client["fyp"]
 col = db["Measurements"]
 insertList = []
 
-
 def importFileToMongo():
 
-    with open('realtime/2020-08-27/1598487787.ndjson') as f:
-        data = ndjson.load(f)
-    col.insert_many(data)
+    pathlist = Path("realtime").glob('**/*.ndjson')
+    for path in pathlist:
+        # because path is object not string
+        path_in_str = str(path)
+        
+        with open(path_in_str) as f:
+            data = ndjson.load(f)
+            col.insert_many(data)
+            print(path_in_str)
+
+
+
 importFileToMongo()
